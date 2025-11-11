979f6c20755672f528389fa33e21bdfdea23a8d9
Guilherme <euguigom@gmail.com>
2025-11-11 16:13:02 -0300

    feat: Add actual YouTube video ID for aespa cover
    - Replaced VIDEO_ID placeholder with fvkk-S3XDEc
    - Video now embedded and ready to display

--- index.php
+++ index.php
@@ -151,7 +151,7 @@
                         <iframe 
                             width="560" 
                             height="315" 
-                            src="https://www.youtube.com/embed/VIDEO_ID" 
+                            src="https://www.youtube.com/embed/fvkk-S3XDEc" 
                             title="aespa Cover" 
                             frameborder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
